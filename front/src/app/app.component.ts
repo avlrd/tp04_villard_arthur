81b2aa2cb8bb2a71af1d7e6c1dca0607e4628933
Arthur <villardarthur@gmail.com>
2024-02-03 17:08:39 +0100

    Header creation

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'TP2';
